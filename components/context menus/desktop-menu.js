c12a193e4b0b910a36560de5a9536ee8ad8ffc6c
Muhammet Âsaf <109434319+sebzviesmaravilha@users.noreply.github.com>
2023-04-05 21:10:27 +0300

    Update desktop-menu.js

--- components/context menus/desktop-menu.js
+++ components/context menus/desktop-menu.js
@@ -45,33 +45,33 @@ function DesktopMenu(props) {
     return (
         <div id="desktop-menu" className={(props.active ? " block " : " hidden ") + " cursor-default w-52 context-menu-bg border text-left font-light border-gray-900 rounded text-white py-4 absolute z-50 text-sm"}>
             <div onClick={props.addNewFolder} className="w-full py-0.5 hover:bg-ub-warm-grey hover:bg-opacity-20 mb-1.5">
-                <span className="ml-5">New Folder</span>
+                <span className="ml-5">Yeni Klasör</span>
             </div>
             <Devider />
             <div className="w-full py-0.5 hover:bg-ub-warm-grey hover:bg-opacity-20 mb-1.5 text-gray-400">
-                <span className="ml-5">Paste</span>
+                <span className="ml-5">Yapıştır</span>
             </div>
             <Devider />
             <div className="w-full py-0.5 hover:bg-ub-warm-grey hover:bg-opacity-20 mb-1.5 text-gray-400">
                 <span className="ml-5">Show Desktop in Files</span>
             </div>
             <div onClick={openTerminal} className="w-full py-0.5 hover:bg-ub-warm-grey hover:bg-opacity-20 mb-1.5">
-                <span className="ml-5">Open in Terminal</span>
+                <span className="ml-5">Terminali Aç</span>
             </div>
             <Devider />
             <div onClick={openSettings} className="w-full py-0.5 hover:bg-ub-warm-grey hover:bg-opacity-20 mb-1.5">
-                <span className="ml-5">Change Background...</span>
+                <span className="ml-5">Arka Planı Değiştir</span>
             </div>
             <Devider />
             <div className="w-full py-0.5 hover:bg-ub-warm-grey hover:bg-opacity-20 mb-1.5 text-gray-400">
-                <span className="ml-5">Display Settings</span>
+                <span className="ml-5">Görüntü Ayarları</span>
             </div>
             <div onClick={openSettings} className="w-full py-0.5 hover:bg-ub-warm-grey hover:bg-opacity-20 mb-1.5">
-                <span className="ml-5">Settings</span>
+                <span className="ml-5">Ayarlar</span>
             </div>
             <Devider />
             <div onClick={goFullScreen} className="w-full py-0.5 hover:bg-ub-warm-grey hover:bg-opacity-20 mb-1.5">
-                <span className="ml-5">{isFullScreen ? "Exit" : "Enter"} Full Screen</span>
+                <span className="ml-5">Tam Ekran{isFullScreen ? "Exit" : "'ı aç"}</span>
             </div>
         </div>
     )
