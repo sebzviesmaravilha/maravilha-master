f7109997204801f91cc235bdebdcac98204174a6
Muhammet Âsaf <109434319+sebzviesmaravilha@users.noreply.github.com>
2023-04-05 21:28:20 +0300

    Update vivek.js

--- components/apps/vivek.js
+++ components/apps/vivek.js
@@ -50,7 +50,7 @@ export class AboutVivek extends Component {
         return (
             <>
                 <div id="about" tabIndex="0" onFocus={this.changeScreen} className={(this.state.active_screen === "about" ? " bg-ub-orange bg-opacity-100 hover:bg-opacity-95" : " hover:bg-gray-50 hover:bg-opacity-5 ") + " w-28 md:w-full md:rounded-none rounded-sm cursor-default outline-none py-1.5 focus:outline-none duration-100 my-0.5 flex justify-start items-center pl-2 md:pl-2.5"}>
-                    <img className=" w-3 md:w-4" alt="about vivek" src="./themes/Yaru/status/about.svg" />
+                    <img className=" w-3 md:w-4" alt="about" src="./themes/Yaru/status/about.svg" />
                     <span className=" ml-1 md:ml-2 text-gray-50 ">Hakkımda</span>
                 </div>
             </>
